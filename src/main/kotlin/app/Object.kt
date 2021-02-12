066714c5a97b274c3cf3fae613eb0e5537141f8e
Kevin Williams Permana <kevinity310@gmail.com>
2021-02-12 16:06:50 +0700

    Belajar Properties

--- src/main/kotlin/app/Object.kt
+++ src/main/kotlin/app/Object.kt
@@ -5,10 +5,17 @@ import data.Person
 fun main() {
     // untuk memmanggil function, cukup memanggil classnya saja
     val kevin = Person()
-    val williams = Person()
-    val permana = Person()
+    kevin.firstName     = "Kevin"
+    kevin.middleName    = "Williams"
+    kevin.lastName      = "Permana"
 
-    println(kevin)
-    println(williams)
-    println(permana)
+    val joko = Person()
+    joko.firstName = "Joko"
+
+    val budi = Person()
+    budi.firstName = "Budi"
+
+    println(kevin.middleName)
+    println(joko.firstName)
+    println(budi.firstName)
 }
