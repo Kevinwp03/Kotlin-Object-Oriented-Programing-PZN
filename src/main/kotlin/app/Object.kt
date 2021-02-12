e60f16fddff73a92cf7d6346e2ff15ce4b9ff92f
Kevin Williams Permana <kevinity310@gmail.com>
2021-02-12 15:29:37 +0700

    Membuat Object

---
+++ src/main/kotlin/app/Object.kt
@@ -0,0 +1,14 @@
+package app
+
+import data.Person
+
+fun main() {
+    // untuk memmanggil function, cukup memanggil classnya saja
+    val kevin = Person()
+    val williams = Person()
+    val permana = Person()
+
+    println(kevin)
+    println(williams)
+    println(permana)
+}
