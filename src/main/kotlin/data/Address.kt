52cdc35d90dac0fd2a5fa4e5c9960a5fc09702f1
Kevin Williams Permana <kevinity310@gmail.com>
2021-02-14 16:39:38 +0700

    Secondary Constructor Without Primary Construction

--- src/main/kotlin/data/Address.kt
+++ src/main/kotlin/data/Address.kt
@@ -1,8 +1,19 @@
 package data
 
 class Address {
+    //harus ada Defaul value
     var street  : String = ""
     var city    : String = ""
     var country : String = ""
 
+    constructor(paramStreet: String, paramCity: String) {
+        street  = paramStreet
+        city    = paramCity
+    }
+
+    constructor(paramStreet: String,paramCity: String, paramCountry:String)
+            : this(paramStreet, paramCity) {
+        // street dan city tidak perlu di panggil karena sudah di set
+                country = paramCountry
+            }
 }
