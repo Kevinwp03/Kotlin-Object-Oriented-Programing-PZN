9d1ae2fcacfddc9c54da7495ca4a7c64c19cfdb0
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-05 22:58:19 +0700

    Belajar HashCode Function

--- src/main/kotlin/data/Company.kt
+++ src/main/kotlin/data/Company.kt
@@ -1,6 +1,10 @@
 package data
 
 class Company (val name: String) {
+
+    override fun hashCode(): Int {
+        return name.hashCode() // mengambil nilai HashCode dari Nilai Objek
+    }
     override fun equals(other: Any?): Boolean {
         return when (other){
             is Company -> other.name == name // maka equal akan membandingkan nilai objek, bukan objeknya
