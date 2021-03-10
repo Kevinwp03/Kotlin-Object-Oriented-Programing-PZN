dd7052956eddda3fac98f8e927bc5ef408df56d4
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-10 14:05:29 +0700

    Concrete Function di Interface

--- src/main/kotlin/data/Interaction.kt
+++ src/main/kotlin/data/Interaction.kt
@@ -3,11 +3,9 @@ package data
 // semua bersifat open
 interface Interaction {
     val name: String // properties
-    fun sayHello(name: String)
-}
-
-class Human(override val name: String): Interaction{
-    override fun sayHello(name: String) {
+    fun sayHello(name: String){
         println("Hello $name, My name is ${this.name}")
     }
 }
+
+class Human(override val name: String): Interaction
