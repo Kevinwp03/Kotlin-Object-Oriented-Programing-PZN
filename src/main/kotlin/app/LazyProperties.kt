ae360d037eadd07a22ecbe0f47d68397cbe94bda
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-20 16:27:12 +0700

    Observable Properties

--- src/main/kotlin/app/LazyProperties.kt
+++ src/main/kotlin/app/LazyProperties.kt
@@ -2,6 +2,7 @@ package app
 import data.Account
 
 fun main() {
+    //lazy Properties
     val account = Account()
     println(account.name)
     println(account.name)
--- src/main/kotlin/data/Account.kt
+++ src/main/kotlin/data/Account.kt
@@ -1,9 +1,16 @@
 package data
 
-class Account {
+import kotlin.properties.Delegates
+
+class Account(description: String = "null") {
     val name: String by lazy{ // Lamda ini di callnya hanya sekali saja, dan nilai lambdanya akan di asign di properties
         //dibawah ini adalah objec delagation objectnya lazy
         println("Name is called")
         "Eko"
+    }
+//Observable Description
+    var description: String by Delegates.observable(description){ // harus var
+        property, oldValue, newValue ->
+        println("${property.name} is changed from $oldValue to $newValue")
     }
 }
