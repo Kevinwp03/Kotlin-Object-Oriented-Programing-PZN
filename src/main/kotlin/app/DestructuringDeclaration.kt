36689628de62f72192b5da4387e0570d362b31d6
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-27 20:01:57 +0700

    Destructuring di Function dan Underscore untuk Variable Tidak Digunakan

--- src/main/kotlin/app/DestructuringDeclaration.kt
+++ src/main/kotlin/app/DestructuringDeclaration.kt
@@ -1,6 +1,14 @@
 package app
 
 import data.Game
+import data.MinMax
+
+fun minmax(value1: Int, value2: Int): MinMax {
+    return when {
+        value1 > value2 -> MinMax(value1, value2)
+        else -> MinMax(value1, value2)
+    }
+}
 
 fun main() {
     val game = Game("Game Kotlin", 100000)
@@ -9,4 +17,13 @@ fun main() {
     val (name, price) = game
     println(name)
     println(price)
+
+    //Destructuring di Function
+//    val result = minmax(100, 10)
+//    val min = result.min
+//    val max = result.max
+
+    val (min, _) = minmax(10, 100)
+    println(min)
+//    println(max)
 }
